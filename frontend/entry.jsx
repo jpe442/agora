0e7b58909e8cca4f73507185aab2b4e848f27a09
Josh <shamanseye@gmail.com>
2017-11-22 15:45:35 -0800

    Add session API utils.

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as SessionAPIUtil from "./util/session_api_util"
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.signup = SessionAPIUtil.signup;
+  window.logout = SessionAPIUtil.logout;
+  window.login = SessionAPIUtil.login;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Agora</h1>, root);
 });
