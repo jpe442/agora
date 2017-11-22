10bca20f570b526c3949188592cb316a09140411
Josh <shamanseye@gmail.com>
2017-11-21 17:33:48 -0800

    Create database, user model, users controller, and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/show'
+
+  get 'users/destroy'
+
+  get 'users/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
