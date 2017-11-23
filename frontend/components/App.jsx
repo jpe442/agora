9fd74da4ed0e4fa9e0cb044b15b981a7864145d5
Josh <shamanseye@gmail.com>
2017-11-22 20:46:36 -0800

    Add navbar and home components and routes.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import NavBarContainer from './navbar/nav_bar_container';
+import Home from './home/home';
+
+
 
 const App = () => (
   <div>
-    <h1>Welcome to Agora Question Forum</h1>
+    <Route path="/" component={NavBarContainer} />
+    <Route exact path="/" component={Home} />
   </div>
 );
 
---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div className="home">
+    <div className="acro-img">
+      <img className="acro-img" src="/assets/images/acro.jpg" />
+    </div> 
+    <h3>Questions First.</h3>
+  </div>
+)
