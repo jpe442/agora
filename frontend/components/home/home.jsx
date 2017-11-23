e21cfde59ae4dd4020ee1e3d222db05f77f89d19
Josh <shamanseye@gmail.com>
2017-11-23 15:13:52 -0800

    Style more navbar/footer/splash

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -6,5 +6,6 @@ export default () => (
       <img className="acro-jpg" src="https://cdn.munplanet.com/storage/uploads/52209627db7c13603b000001/image/data/564c58dda2a029f64b054b06/large_Acropolis-Of-Athens-9.jpg" />
     </div> 
     <h3 className="tagline">Questions First.</h3>
+    <div className="footer">Josh Earlenbaugh</div>
   </div>
 )
