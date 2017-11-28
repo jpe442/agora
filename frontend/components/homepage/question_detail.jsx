a0231e4115aa29717605c802ccb969bd3d713cad
Josh <shamanseye@gmail.com>
2017-11-28 15:48:28 -0800

    Sytle question detail more...

--- frontend/components/homepage/question_detail.jsx
+++ frontend/components/homepage/question_detail.jsx
@@ -16,11 +16,13 @@ class QuestionDetail extends React.Component {
     console.log(this.props)
     return (
       <div>
-        <h2 className="question-detail-title">{this.props.question.title}</h2>
-        <h3 className="interlocutor-name">Interlocutor: {this.props.question.interlocutor_id}</h3>
-        <p className="question-detail-body">{this.props.question.body}</p>
-        <Link className="detail-to-homepage" to={"/homepage/"}>go back</Link>
-        <h4>give me a way to edit this question if I am the interlocutor</h4>
+        <div className="question-detail-main">
+          <h2 className="question-detail-title">{this.props.question.title}</h2>
+          <h3 className="interlocutor-name">Interlocutor: {this.props.question.interlocutor_id}</h3>
+          <p className="question-detail-body">{this.props.question.body}</p>
+          <Link className="detail-to-homepage" to={"/homepage/"}>go back</Link>
+          <h4>give me a way to edit this question if I am the interlocutor</h4>
+        </div>
     
       </div>
     )
